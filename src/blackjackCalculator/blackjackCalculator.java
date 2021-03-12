b2d73029cc82f35caf42b0e571ade9238eda898e
Mikael Rouhiainen <mikael.rouhiainen@bitsinglass.com>
2021-03-12 13:07:05 -0700

    fixes for buttons

--- src/blackjackCalculator/blackjackCalculator.java
+++ src/blackjackCalculator/blackjackCalculator.java
@@ -3,58 +3,15 @@ package blackjackCalculator;
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JFrame;
-import javax.swing.ImageIcon;
  
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 public class blackjackCalculator extends JPanel implements ActionListener {
-	public void run() {
-		JButton two;
-		JButton three;
-		JButton four;
-		JButton five;
-		JButton six;
-		JButton seven;
-		JButton eight;
-		JButton nine;
-		JButton ten;
-		JButton ace;
-	two = new JButton("2");
-	two.setVerticalTextPosition(AbstractButton.CENTER);
-	two.setMnemonic(KeyEvent.VK_2);
-	two.setActionCommand("flip");
-	two.addActionListener(this);
-	three = new JButton("3");
-	three.setVerticalTextPosition(AbstractButton.CENTER);
-	three.setMnemonic(KeyEvent.VK_3);
-	four = new JButton("4");
-	four.setVerticalTextPosition(AbstractButton.CENTER);
-	five = new JButton("5");
-	five.setVerticalTextPosition(AbstractButton.CENTER);
-	six = new JButton("6");
-	six.setVerticalTextPosition(AbstractButton.CENTER);
-	seven = new JButton("7");
-	seven.setVerticalTextPosition(AbstractButton.CENTER);
-	eight = new JButton("8");
-	eight.setVerticalTextPosition(AbstractButton.CENTER);
-	nine = new JButton("9");
-	nine.setVerticalTextPosition(AbstractButton.CENTER);
-	ten = new JButton("10/J/Q/K");
-	ten.setVerticalTextPosition(AbstractButton.CENTER);
-	ace = new JButton("A");
-	ace.setVerticalTextPosition(AbstractButton.CENTER);
 	
-	add(two);
-	add(three);
-	add(four);
-	add(five);
-	add(six);
-	add(seven);
-	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
@@ -65,28 +22,55 @@ public class blackjackCalculator extends JPanel implements ActionListener {
 		}
 	}
 	 private static void createAndShowGUI() {
+		 JTextField decks = new JTextField();
+		 JButton two = new JButton("2");
+		 JButton three = new JButton("3");
+		 JButton four = new JButton ("4");
+		 JButton five = new JButton("5");
+		 JButton six = new JButton("6");
+		 JButton seven = new JButton("7");
+		 JButton eight = new JButton("8");
+		 JButton nine = new JButton("9");
+		 JButton ten = new JButton("10/J/Q/K");
+		 JButton ace = new JButton("A");
 		 
-	        //Create and set up the window.
-	        JFrame frame = new JFrame("Blackjack Calculator");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 
-	        //Create and set up the content pane.
-	        blackjackCalculator newContentPane = new blackjackCalculator();
-	        newContentPane.setOpaque(true); //content panes must be opaque
-	        frame.setContentPane(newContentPane);
-	 
-	        //Display the window.
-	        frame.pack();
-	        frame.setVisible(true);
+
+		 //Create and set up the window.
+		 JFrame frame = new JFrame("Blackjack Calculator");
+		 frame.setSize(800,795);
+		 two.setBounds(50,40,65,30);
+		 three.setBounds(120,40,65,30);
+		 four.setBounds(190,40,65,30);
+		 five.setBounds(260,40,65,30);
+		 six.setBounds(330,40,65,30);
+		 seven.setBounds(400,40,65,30);
+		 eight.setBounds(470,40,65,30);
+		 nine.setBounds(540,40,65,30);
+		 ten.setBounds(610,40,65,30);
+		 ace.setBounds(680,40,65,30);
+		 frame.add(two);
+		 frame.add(three);
+		 frame.add(four);
+		 frame.add(five);
+		 frame.add(six);
+		 frame.add(seven);
+		 frame.add(eight);
+		 frame.add(nine);
+		 frame.add(ten);
+		 frame.add(ace);
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		 //Display the window.
+		 frame.setLayout(null);
+		 frame.setVisible(true);
 	    }
 	 public static void main(String[] args) {
 	        //Schedule a job for the event-dispatching thread:
 	        //creating and showing this application's GUI.
-	        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                createAndShowGUI(); 
-	            }
-	        });
+	            
+	        createAndShowGUI(); 
+	            
 	    }
 	
 }
